4dd70ae69b5d1b60fbfba6b0be266410ee57807a
9yubean2 <dlrbqls3024@swu.ac.kr>
2023-06-08 11:03:07 +0900

    Part03. 코딩테스트 문제 풀이/Ch01. 자료구조 - 해설

--- Python/Part03. codingtest_problemsolving/Ch01.자료구조/07. 📌 BOJ_7662.py
+++ Python/Part03. codingtest_problemsolving/Ch01.자료구조/07. 📌 BOJ_7662.py
@@ -15,22 +15,28 @@ input = sys.stdin.readline
 test_case = int(input())
 
 for _ in range(test_case):
+    
     k = int(input())
     min_heap = []   #* 최소 힙 (heapq는 최소 힙)
     max_heap = []   #* 최대 힙 (삽입/삭제 시 음수로 삽입)
     current = 0     #* 삽입할 원소의 인덱스 값
     deleted = [False] * (k + 1)
+    
     for i in range(k):
         command = input().split()
         operator, data = command[0], int(command[1])
+        
         if operator == 'D':
             if data == -1: pop(min_heap)
             elif data == 1: pop(max_heap)
+        
         elif operator == 'I':
             heapq.heappush(min_heap, (data, current))
             heapq.heappush(max_heap, (-data, current))
             current += 1
+    
     max_value = pop(max_heap)
+    
     if max_value == None: print("EMPTY")
     else:
         heapq.heappush(min_heap, (-max_value, current))
